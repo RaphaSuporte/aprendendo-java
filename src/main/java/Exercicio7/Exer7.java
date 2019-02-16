a3ff56b791a7365a54ca25ddea2f00cd05831c1c
Raphael Andrdrade <rapha.suporte@gmail.com>
2019-02-16 19:20:38 -0200

    [raphael][Exercicio7]Completo

--- src/main/java/Exercicio7/Exer7.java
+++ src/main/java/Exercicio7/Exer7.java
@@ -9,18 +9,20 @@ public class Exer7 {
 
         Random mat = new Random();
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 1; j++) {
 
             }
 
         }
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(matriz[i][j] + " ");
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.println("00 01 02 03 04 \n");
+                System.out.println("20 21 22 23 24 \n");
+                System.out.println("30 31 32 33 34 \n");
+                System.out.println("40 41 42 43 44 ");
             }
-            System.out.println("\n");
         }
 
     }
